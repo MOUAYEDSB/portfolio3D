8f26e15a57e74cb72bbf32444cb66432537115ab
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-11-08 17:56:53 +0100

    add project

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="h-full w-full flex flex-wrap gap-10 px-10 justify-center">
         <ProjectCard
           src="/projrt-1.webp"
           title="HealthCab website"
@@ -23,8 +23,13 @@ const Projects = () => {
         />
          <ProjectCard
           src="/webrtc.png"
-          title="3D portfolio website"
-          description="3D Portfolio! Dive into a world where imagination meets innovation. From stunning visualizations and realistic renderings to captivating animations."
+          title="Real-Time Communication"
+          description="WebRTC and Socket.io project enables real-time, peer-to-peer communication, allowing users to seamlessly engage in video, audio, and data exchanges over the web. By integrating WebRTC with Socket.io for efficient signaling and event-driven communication."
+        />
+         <ProjectCard
+          src="/project4.png"
+          title="Interactive E-Learning Platform for Kids"
+          description="Graines d'Entrepreneur is an engaging e-learning platform designed to cultivate entrepreneurial skills in young learners. Tailored specifically for children, it combines interactive learning modules, creative exercises, and hands-on activities that inspire critical thinking and innovation"
         />
         <ProjectCard
           src="/projects-3.png"
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -9,18 +9,18 @@ interface Props {
 
 const ProjectCard = ({ src, title, description }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] w-80 h-auto transform transition-transform duration-300 hover:scale-105">
       <Image
         src={src}
         alt={title}
-        width={1000}
-        height={1000}
-        className="w-full object-contain"
+        width={320}
+        height={192}
+        className="w-full h-48 object-cover"
       />
 
       <div className="relative p-4">
-        <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+        <h1 className="text-xl font-semibold text-white">{title}</h1>
+        <p className="mt-2 text-gray-300 text-sm">{description}</p>
       </div>
     </div>
   );
